8fc1e1f49ac0f0edeceaae571c0726d683a72131
Dmitry Golovinov <dmitry.golovinov@jetbrains.com>
2022-10-26 10:35:02 +0200

    Add new example for the blog post

--- AsyncVoid.cs
+++ AsyncVoid.cs
@@ -1,6 +1,11 @@
 public class AsyncVoid {
   void Method()
   {
+      RunThisAction(async () =>
+      {
+          await Task.Delay(1000);
+          throw new NotImplementedException();
+      });
       RunThisAction(async () => await Task.Delay(1000));
       // or
       RunThisAction(async delegate { await Task.Delay(1000); });
